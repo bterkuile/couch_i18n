14e4432ff01ba8d7f7a9b52cfe198c311b0e6559
Benjamin ter Kuile <bterkuile@gmail.com>
2012-12-17 17:15:06 +0100

    allow for faster switching between with and without cmtool testing

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include SimplyStored::Couch
-  include Devise::Orm::SimplyStored
+  include Devise::Orm::SimplyStored if defined? Cmtool
 
   property :email
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable if defined? Cmtool
 
   validates_presence_of :email
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users #, :controllers => {:sessions => 'cmtool/sessions', :passwords => 'cmtool/passwords'}
+  #devise_for :users #, :controllers => {:sessions => 'cmtool/sessions', :passwords => 'cmtool/passwords'}
   mount CouchI18n::Engine => "/couch_i18n", as: 'couch_i18n'
   mount Cmtool::Engine => '/cmtool', as: 'cmtool'
 end
