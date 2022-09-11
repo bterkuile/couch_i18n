9c5b571cf26835e1d4ac4d12e1f803caab827893
Benjamin ter Kuile <bterkuile@gmail.com>
2022-09-11 08:27:36 -0500

    Explicitly require model in engine initializer for rails 7 changes

--- lib/couch_i18n/engine.rb
+++ lib/couch_i18n/engine.rb
@@ -5,6 +5,8 @@ module CouchI18n
     initializer 'couch_i18n.cmtool', after: 'cmtool.build_menu' do
       if defined? Cmtool
         require 'cmtool'
+        require 'couch_i18n/translation'
+
         Cmtool::Menu.register do
           append_to :site do
             resource_link CouchI18n::Translation, label: :couch_i18n
